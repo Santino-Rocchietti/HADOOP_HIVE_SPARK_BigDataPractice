0538a9f24cb810fb6acfd36de0370bb561cf6b69
Santino-Rocchietti <157161478+Santino-Rocchietti@users.noreply.github.com>
2024-09-07 04:12:52 -0300

    Ejercicio2.sh

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -1,18 +1,9 @@
-# Primero borro si ya existe para cuando hago cambios
-sudo docker exec -it hive-server bash -c "
-cd hive
-cd scripts
-rm Paso02.hql
-cd ..
-cd ..
-exit
-"
 
-# Copio el hql al contenedor
+# Copiar el hql al contenedor
 sudo docker cp Paso02.hql hive-server:/opt/hive/scripts/Paso02.hql
-# Ejecuto el contenedor
+# Ejecutar el contenedor
 sudo docker exec -it hive-server bash
-# Ejecuto el hql desde el contenedor
+# Ejecutar el hql desde el contenedor
 hive -f /opt/hive/scripts/Paso02.hql
-# Salgo del contenedor
+# Salir del contenedor
 sudo docker exec -it hive-server bash -c "exit"
